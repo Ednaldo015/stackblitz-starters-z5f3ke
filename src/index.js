6a298ba6fae9b2d1ef6e1615186622f11a7fc125
Ednaldo015 <132470028+Ednaldo015@users.noreply.github.com>
2023-06-19 03:46:17 -0300

    5 files added

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-data-grid/lib/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { useScrollToTop as ScrollToTop } from 'hooks';
+import { Provider } from 'context/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import { Analytics } from "components";
+import App from './App';
+
+function Loja() {
+	return (
+		<BrowserRouter basename="/">
+			{process.env.NODE_ENV === "production" && <Analytics />}
+			<Provider>
+				<ScrollToTop />
+				<ToastContainer />
+				<App />
+			</Provider>
+		</BrowserRouter>
+	);
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Loja /
